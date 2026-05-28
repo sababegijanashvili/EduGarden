14a2e515874ec05e6fd4e40cfed46641561803b1
Saba <sababegijanashvili@gmail.com>
2026-05-28 16:05:31 +0400

    fix: admin panel all sections working

---
+++ admin.js
@@ -0,0 +1,4 @@
+
+    const SUPABASE_URL = 'https://exoyfpqfgixofsuginxx.supabase.co';
+    const SUPABASE_KEY = 'sb_publishable_UExfTnoTaFgieljt6oiZ8Q_m0f2raxy';
+    const supabase = supabaseJs.createClient(SUPABASE_URL, SUPABASE_KEY);
